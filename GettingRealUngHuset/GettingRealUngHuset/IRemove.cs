9a30d52c6eeaeae1e157cf27d6f1ce1f50879e9a
Andreas Juul Willumsen <andr532c@edu.eal.dk>
2018-02-22 12:53:57 +0100

    iadd iremove added

---
+++ GettingRealUngHuset/GettingRealUngHuset/IRemove.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealUngHuset
+{
+    interface IRemove
+    {
+        void RemoveItem(string ItemID);
+    }
+}
---
+++ GettingRealUngHuset/GettingRealUngHuset/Iadd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealUngHuset
+{
+    interface Iadd
+    {
+        void AddMaterial(String ItemName, string ItemID);
+        
+
+        
+    }
+}
